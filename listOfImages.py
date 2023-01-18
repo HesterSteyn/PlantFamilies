6dc364a801ff38a1dd39ae7a14ea56bd91608ccf
Hester <h.steyn@sanbi.org.za>
2023-01-18 21:51:37 +0200

    moved from listdir to os.walk

--- listOfImages.py
+++ listOfImages.py
@@ -3,20 +3,25 @@ import csv
 from csv import reader
 from pathlib import Path
 
-imagedir = 'G:/NBG_NonTypes'
-imageFile = 'ListScannedSpecimensOnDisk_NBG_NonTypes.csv'
+imagedir = r'H:\2023 Copy CPF images_Names corrected'
+imageFile = 'temp.csv'
+filetypes = ['.jpg', '.jpeg', '.tif', '.tiff']
 
 def imageNames(dir):
-    imageList = []
-    images = os.listdir(dir)
-    for image in images:
-        image = Path(image).stem
-        if image[0].isdigit(): #exclude images with only a number as a filename, these are diagnostics
+  imageList = []
+  for root, dirs, files in os.walk(dir):
+    for file in files:
+      fileExt = Path(file).suffix
+      if fileExt.lower() in filetypes:
+        if file[0].isdigit(): #exclude images starting with a number in a filename
           continue
         else:
-          imageList.append(image)
-        # print(imageList)
-    return imageList
+          imageList.append(Path(file).stem)
+      
+  uniques = set(imageList)
+  print(len(imageList), 'images in total')
+  print(len(uniques), 'unique image names')
+  return imageList
 
 listImages = imageNames(imagedir)
 
